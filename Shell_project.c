271f3ce1aaf0d7955f51dff20ba09baf28275136
LadyNightmare <ladynightmare2@outlook.com>
2018-06-06 20:42:24 +0200

    Finilly finished!

--- Shell_project.c
+++ Shell_project.c
@@ -15,6 +15,7 @@ To compile and run the program:
 **/
 
 #include "job_control.h"   // remember to compile with module job_control.c 
+#include <string.h>
 
 #define MAX_LINE 256 /* 256 chars per line, per command, should be enough. */
 
@@ -43,6 +44,8 @@ int main(void) {
 
 	while (1) {   /* Program terminates normally inside get_command() after ^D is typed*/
 
+		//Recorrer la lista e imprimir los que están hechos
+
 		printf("COMMAND->");
 		fflush(stdout);
 		get_command(inputBuffer, MAX_LINE, args, &background);  /* get next command */
@@ -55,23 +58,86 @@ int main(void) {
 
 				chdir(args[1]);
 
+			} else if (strcmp(args[0], "jobs") == 0) {
+
+				print_job_list(jobs);
+
+			} else if(strcmp(args[0], "fg") == 0) {
+
+				job* job;
+
+				if (args [1] == NULL) {
+
+					job = get_item_bypos(jobs, 1);
+
+				} else {
+
+					job = get_item_bypos(jobs, atoi(args[1]));
+
+				}
+
+				set_terminal(job->pgid);
+
+				//We change its state to foreground
+
+				job->state = FOREGROUND;
+
+				//We get the id of the group and we send the signal to continue (SIGCONT)
+
+
+				printf("%s\n", job->command);
+
+				killpg(job->pgid, 18);
+
+				int pid = waitpid(job->pgid, &status, WUNTRACED);
+
+				set_terminal(getpid());
+
+				//We analyze its status and if it's finished a pastar Maricarmen
+				//and if not, pues a pararlo
+
+				status_res = analyze_status(status, &info);
+
+				if (status_res == EXITED || status_res == SIGNALED) {
+
+					delete_job(jobs, job);
+
+				} else if (status_res == SUSPENDED) {
+
+					job->state = STOPPED;
+
+				}
+
+			} else if (strcmp(args[0], "bg") == 0) {
+
+				job* job;
+
+				if (args [1] == NULL) {
+
+					job = get_item_bypos(jobs, 1);
+
+				} else {
+
+					job = get_item_bypos(jobs, atoi(args[1]));
+
+				}
+
+				//We change the state of the job to background and we start it again
+
+				job->state = BACKGROUND;
+				killpg(job->pgid, 18);
+
 			} else {
 
 				pid_fork = fork(); //We create the child
 
 							if(!pid_fork) { //This is the child
 
-								new_process_group(pid_fork); //We create the process group for the child
+								new_process_group(getpid()); //We create the process group for the child
 
-								if(!background) {
+								if(!background) { //If not in background
 
-									set_terminal(pid_fork); //We give the control of the terminal to the child process in foreground
-
-								} else {
-
-									job process = new_job(pid_fork, *args[0], FOREGROUND);
-
-									add_job(&jobs, process);
+									set_terminal(getpid()); //We give the control of the terminal to the child process in foreground
 
 								}
 
@@ -83,18 +149,33 @@ int main(void) {
 
 							} else { //This is the father
 
+								new_process_group(pid_fork);
+
 								if (!background) { //If it's in background
 
-									waitpid(pid_fork, &status, WUNTRACED, WNOHANG ); //Wait for the child [0 to work normally]
 
 									set_terminal(pid_fork); //After the child has finished, we recover the control of the terminal
 
+									waitpid(pid_fork, &status, WUNTRACED); //Wait for the child [0 to work normally]
+
+									set_terminal(getpid());
+
 									status_res = analyze_status(status, &info);
-									printf("Foregroung pid: %d, command: %s, %s, info: %d\n", pid_fork, args [0], status_strings[status_res], info);
+									if (info != 255) {
 
+										printf("Foregroung pid: %d, command: %s, %s, info: %d\n", pid_fork, args [0], status_strings[status_res], info);
 
+									}
+
+									if (status_res == SUSPENDED) {
+
+										add_job(jobs, new_job(pid_fork, args[0], STOPPED));
+
+									}
 
 								}else {
+
+									add_job(jobs, new_job(pid_fork, args[0], BACKGROUND));
 
 									printf("Background job running... pid: %d, command: %s\n", pid_fork, args [0]);
 
@@ -115,36 +196,77 @@ int main(void) {
 
 	} // end while
 
-	void sighandler (int signal) {
+}
 
-		int pos = 0;
+void sighandler (int signal) {
 
-		job* job = get_item_bypos(&jobs, 0);
+	block_SIGCHLD();
 
-		while(job != NULL) {
+	int pos = 0;
 
-			int pid = waitpid(job->pgid, WNOHANG);
+	int status, info;
+	enum status status_res;
+
+	//We get the first job
+
+	job* job = get_item_bypos(jobs, 1);
+
+	while(job != NULL) {
+
+		if(job->state != FOREGROUND) {
+
+			int pid = waitpid(job->pgid, &status, WUNTRACED | WNOHANG);
 
 			if (pid == job->pgid) {
 
-				status_res = analyze_status(job->state, &info);
+				//If the pid is the one of the process that has sent the signal, we analyze its status
 
-				if (status_res == EXITED) {
+				status_res = analyze_status(status, &info);
 
-					delete_job(&jobs, job);
-					printf(" Job %s has finished its execution.\n", job->command);
+				if (status_res == EXITED || status_res == SIGNALED) {
+
+					//If it's finished, we delete it from the list
+
+					job->state = STOPPED;
+					delete_job(jobs, job);
+					printf("Job %s has finished its execution.\n", job->command);
 
 				} else if (status_res == SUSPENDED) {
+
+					//If it's suspended, we change its state
 
 					job->state = STOPPED;
 					printf("Job %s has been suspended.\n", job->command);
 
 				}
 
+
+				//We get the next job
+
+				++pos;
+				job = get_item_bypos(jobs, pos);
+
+			} else {
+
+				//We get the next job
+
+				++pos;
+				job = get_item_bypos(jobs, pos);
+
 			}
+
+		} else {
+
+			//We get the next job
+
+			++pos;
+			job = get_item_bypos(jobs, pos);
 
 		}
 
+
 	}
+
+	unblock_SIGCHLD();
 
 }
